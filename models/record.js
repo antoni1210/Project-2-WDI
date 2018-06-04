197a6ef0754c123a15f8f0d3df0c0ca2fba9ad09
Antoni Alston <sntonialston@gmail.com>
2018-06-04 11:00:25 +0100

    updated model for records to handle user comments

--- models/record.js
+++ models/record.js
@@ -5,9 +5,15 @@ const recordSchema = new mongoose.Schema({
   artist: String,
   format: String,
   price: Number,
-  image: String
+  image: String,
+  comments: [commentSchema]
 }, {
   timestamps: true
+});
+
+const commentSchema = new mongoose.Schema({
+  content: String,
+  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
 });
 
 module.exports = mongoose.model('Record', recordSchema);
